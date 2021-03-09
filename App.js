8c058c162be3a0952a7df31a7f8d771b06192080
CharleyDvs <ing.carlosjaime@outlook.com>
2021-03-09 11:01:23 -0600

    Implemented a theme and added theme colors

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
--- src/features/restaurants/components/RestaurantInfoCard.js
+++ src/features/restaurants/components/RestaurantInfoCard.js
@@ -3,14 +3,14 @@ import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 
 const Title = styled.Text`
-  padding: 20px;
-  font-weight: 700;
-  font-size: 20px;
-  color: #222;
+  padding: ${(props) => props.theme.space[2]};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+  font-size: ${(props) => props.theme.fontSizes.title};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const CardContent = styled(Card.Content)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
