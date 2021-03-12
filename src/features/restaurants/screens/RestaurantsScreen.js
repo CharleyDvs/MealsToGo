d46662e8a65f68d9fac5e5b2f1f859d3dc04e20f
CharleyDvs <ing.carlosjaime@outlook.com>
2021-03-11 20:34:24 -0600

    Set a geocoding service with mock data

--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import styled from "styled-components/native";
 import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
@@ -18,6 +19,13 @@ const RestaurantFlatlist = styled(FlatList)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const StatusIconContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  transform: scale(3);
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
@@ -25,13 +33,20 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
-      <RestaurantFlatlist
-        data={restaurants}
-        renderItem={({ item }) => {
-          return <RestaurantInfoCard restaurant={item} />;
-        }}
-        keyExtractor={(item) => item.name}
-      />
+      {isLoading && (
+        <StatusIconContainer>
+          <ActivityIndicator animating={true} color={Colors.black} />
+        </StatusIconContainer>
+      )}
+      {!isLoading && !error && (
+        <RestaurantFlatlist
+          data={restaurants}
+          renderItem={({ item }) => {
+            return <RestaurantInfoCard restaurant={item} />;
+          }}
+          keyExtractor={(item) => item.name}
+        />
+      )}
     </SafeArea>
   );
 };
