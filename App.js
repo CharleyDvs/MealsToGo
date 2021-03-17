03d40dc343c7a1fbdcea58d3b9b5327189f66b87
CharleyDvs <ing.carlosjaime@outlook.com>
2021-03-16 19:59:07 -0600

    Set up an authentication context using firebase

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import * as firebase from "firebase";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthContextProvider } from "./src/services/authentication/authentication.context";
 import { Navigation } from "./src/infrastructure/navigation";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBa_CtrSB5tXROxU5vgqp4-0aww8XEBqvI",
+  authDomain: "mealstogo-4f38d.firebaseapp.com",
+  projectId: "mealstogo-4f38d",
+  storageBucket: "mealstogo-4f38d.appspot.com",
+  messagingSenderId: "1010292509224",
+  appId: "1:1010292509224:web:e39beefc9eceda3dcb431d",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 import {
   useFonts as useOswald,
@@ -32,16 +47,19 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthContextProvider>
+          <FavouritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavouritesContextProvider>
+        </AuthContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
---
+++ src/services/authentication/authentication.context.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import * as firebase from "firebase";
+import { createContext } from "react";
+
+import { loginRequest } from "./authentication.services";
+
+export const AuthenticationContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const onLogin = (email, password) => {
+    setIsLoading(true);
+    loginRequest(email, password)
+      .then((userObject) => {
+        setUser(userObject);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setError(e);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        user,
+        isLoading,
+        error,
+        onLogin,
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
